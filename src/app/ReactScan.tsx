6e511768e43840555a54306f1fa7dde097e93f39
Catalin Glavan <glmihaicata@gmail.com>
2025-06-23 01:20:01 +0300

    move assets

--- src/app/ReactScan.tsx
+++ src/app/ReactScan.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const ReactScan: React.FC = () => {
-    const env = import.meta.env.VITE_APP_STAGE;
+    const {MODE} = import.meta.env;
 
-    return env === 'local' && <script async src='//unpkg.com/react-scan/dist/auto.global.js' />;
+    return MODE === 'dev' && <script async src='//unpkg.com/react-scan/dist/auto.global.js' />;
 };
 
 export default ReactScan;
